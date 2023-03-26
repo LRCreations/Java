bba498f199ca6eb877ad27d510c716bf8dffcfac
LRCreations <lr20company21@gmail.com>
2023-03-26 11:03:37 +0530

    Java Method Overloading Project Code

---
+++ src/Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+    void sum(int a, long b) {
+        System.out.println(a + b);
+    }
+    void sum(int c, int d) {
+        System.out.println(c + d);
+    }
+    public static void main(String[] args) {
+        Sum s = new Sum();
+        s.sum(1, 123456789L);
+        s.sum(2, 2468);
+    }
+}
